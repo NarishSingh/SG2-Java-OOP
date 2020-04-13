a695b97f6fbdd6ca1b7cd0a5aff8bd8655e19986
Narish Singh <narish.singh786@gmail.com>
2020-04-13 14:12:39 -0400

    Added Assignment 1 - user name and age, with age conversions

---
+++ Milestone1/src/main/java/Assignment1UserNameAge/UserNameAge.java
@@ -0,0 +1,38 @@
+/* Take user name and age 
+ * convert age to years, months, days, hours, seconds, and milliseconds
+ */
+package Assignment1UserNameAge;
+
+import java.util.Scanner;
+
+public class UserNameAge {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //inputs
+        System.out.println("Please enter your name:");
+        String name = input.nextLine();
+
+        System.out.println("Please enter your age:");
+        int ageYrs = input.nextInt();
+        
+        //conversions
+        int ageMonth = ageYrs * 12;
+        int ageDays = ageYrs * 365;
+        int ageHrs = ageDays * 24;
+        long ageMin = ageHrs * 60;
+        long ageSec = ageMin * 60;
+        long ageMs = ageSec * 1000;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age in years: " + ageYrs);
+        System.out.println("Age in months: " + ageMonth);
+        System.out.println("Age in days: " + ageDays);
+        System.out.println("Age in hours: " + ageHrs);
+        System.out.println("Age in minutes: " + ageMin);
+        System.out.println("Age in seconds: " + ageSec);
+        System.out.println("Age in milliseconds: " + ageMs);
+    }
+
+}
