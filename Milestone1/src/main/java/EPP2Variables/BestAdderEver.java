8c70339df888a8cecd34c35aece143f9b4f8e98c
Narish Singh <narish.singh786@gmail.com>
2020-04-14 14:49:40 -0400

    fixed BestAdderEver, was supposed to be hard coded not Scanner

--- Milestone1/src/main/java/EPP2Variables/BestAdderEver.java
+++ Milestone1/src/main/java/EPP2Variables/BestAdderEver.java
@@ -5,24 +5,16 @@
  */
 package EPP2Variables;
 
-import java.util.Scanner;
-
 public class BestAdderEver {
 
+    
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int sum;
-
-        System.out.print("Enter first number: ");
-        int num1 = input.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = input.nextInt();
-        System.out.print("Enter third number: ");
-        int num3 = input.nextInt();
-
-        sum = num1 + num2 + num3;
+        int num1=1;
+        int num2=2;
+        int num3=3;
         
+        int sum=num1+num2+num3;
         System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + sum);
     }
-
+    
 }
