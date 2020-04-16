fb5e3096f3d2bb6013e37713bcced8c2a4a5aec9
Narish Singh <narish.singh786@gmail.com>
2020-04-16 15:02:42 -0400

    fixed stay positive so it displays as required

--- Milestone1/src/main/java/EPP6WhileDoLoops/StayPositive.java
+++ Milestone1/src/main/java/EPP6WhileDoLoops/StayPositive.java
@@ -1,6 +1,6 @@
 /**
  * write a while loop that counts down to 0, no negatives
- * print rows of 10's then newline - need to figure out how to do
+ * print rows of 10's then newline
  * throw error if negative
  */
 package EPP6WhileDoLoops;
@@ -18,6 +18,7 @@ public class StayPositive {
             i--;
         }
 
+        int counter = 0;
         System.out.print("Enter starting value: ");
         int j = input.nextInt();
         if (j < 0) {
@@ -25,10 +26,8 @@ public class StayPositive {
         }
 
         while (j >= 0) {
-            int counter = 0;
-            
             if (counter%10==0) {
-                System.out.print("\n" + j);
+                System.out.print("\n" + j + " ");
                 counter++;
                 j--;
             } else {
