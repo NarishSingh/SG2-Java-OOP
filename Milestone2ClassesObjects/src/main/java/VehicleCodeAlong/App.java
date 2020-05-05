c2bbc6aaeee3a4d76f743bf53c2d7e141c556422
Narish Singh <narish.singh786@gmail.com>
2020-05-05 15:32:24 -0400

    final

--- Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/App.java
+++ Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/App.java
@@ -6,6 +6,7 @@ In-Class assignment
  */
 package VehicleCodeAlong;
 
+import java.io.*;
 import java.util.*;
 
 public class App {
@@ -13,61 +14,68 @@ public class App {
     public static void main(String[] args) {
         Vehicle v = new Vehicle("Generic Car", "White", 50);
         v.gas();
-        
+
         //cars
 //        Car toyotaCamry = new Car("Camry", "Red", 125, false, "Toyota", 1998, 4, true, 15);
         Car toyotaCamry = new Car(15, "Camry", "Blue", 150);
         Car ferrariEnzo = new Car(60, "Enzo", "Red", 240);
-        
+
         //planes
-        Plane bob=new Plane(500, "Bob", "Blue", 1000);
-        Plane airbus=new Plane(600, "Airbus", "Purple", 3000);
-        Plane boeing=new Plane(700, "Boeing", "Red", 5000);
+        Plane bob = new Plane(500, "Bob", "Blue", 1000);
+        Plane airbus = new Plane(600, "Airbus", "Purple", 3000);
+        Plane boeing = new Plane(700, "Boeing", "Red", 5000);
 
         //drive
         toyotaCamry.gas();
         toyotaCamry.gas();
         toyotaCamry.gas();
         toyotaCamry.brake();
-        
+
         ferrariEnzo.gas();
         ferrariEnzo.gas();
         ferrariEnzo.gas();
         ferrariEnzo.gas();
         ferrariEnzo.gas();
         ferrariEnzo.brake();
-        
+
         bob.takeoff();
         bob.land();
-        
+
         //HashMap
-        Map<Integer, Plane> planeHangar=new HashMap<>();
-        
+        Map<Integer, Plane> planeHangar = new HashMap<>();
+
         //put obj's to HashMap
         planeHangar.put(0, bob);
         planeHangar.put(1, airbus);
         planeHangar.put(2, boeing);
-        
+
         //keySet
-        Set<Integer> planeKeys=planeHangar.keySet(); //keyset
-        ArrayList<Plane> hangarRecord=new ArrayList<>(); //plane records
-        
+        Set<Integer> planeKeys = planeHangar.keySet(); //keyset
+        ArrayList<Plane> hangarRecord = new ArrayList<>(); //plane records
+
         for (Integer pk : planeKeys) {
             hangarRecord.add(planeHangar.get(pk)); //added plane records to List
         }
-        
-        //Make into a Strings
-        ArrayList<String> hangarRecString=new ArrayList<>();
-        
+
+        //Make into a Strings --> Marshall
+        ArrayList<String> hangarRecString = new ArrayList<>();
+
         for (Plane plane : hangarRecord) {
             String aPlane = PlaneRecord.saveRecord(plane);
-            
+
             hangarRecString.add(aPlane);
         }
-        
+
         System.out.println("");
         for (String planeString : hangarRecString) {
             System.out.println(planeString);
+        }
+
+        System.out.println("");
+        //return to obj --> unmarshall
+        for (String aPlane : hangarRecString) {
+            Plane p = PlaneRecord.unloadRecord(aPlane);
+            p.takeoff();
         }
     }
 
--- Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/PlaneRecord.java
+++ Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/PlaneRecord.java
@@ -13,15 +13,23 @@ public class PlaneRecord {
         String planeString = "";
 
         //cast everything to String
-        planeString += p.getName() + DELIMITER;
-        planeString += p.getColor() + DELIMITER;
+        planeString += p.getName() + DELIMITER; //0
+        planeString += p.getColor() + DELIMITER; //1
 //        planeString += Double.toString(p.getMaxSpeed()) + DELIMITER;
 //        planeString += Double.toString(p.getAccel());
-        planeString += p.getMaxSpeed() + DELIMITER;
-        planeString += p.getAccel();
+        planeString += p.getMaxSpeed() + DELIMITER; //2
+        planeString += p.getAccel(); //3
         
         return planeString;
     }
     
-    //unmarshalling - later
+    //unmarshalling
+    public static Plane unloadRecord(String planeString){
+        String[] planeData=planeString.split(DELIMITER);
+        
+        //parse
+        Plane plane = new Plane(Double.parseDouble(planeData[3]), planeData[0], planeData[1], Double.parseDouble(planeData[2]));
+        
+        return plane;
+    }
 }
