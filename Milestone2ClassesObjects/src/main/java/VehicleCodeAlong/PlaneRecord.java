7c5eceeaa9045a1b4a6c9eafaf95c2ad03beec8e
Narish Singh <narish.singh786@gmail.com>
2020-05-05 21:17:42 -0400

    Commented out the shorthand

--- Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/PlaneRecord.java
+++ Milestone2ClassesObjects/src/main/java/VehicleCodeAlong/PlaneRecord.java
@@ -13,12 +13,12 @@ public class PlaneRecord {
         String planeString = "";
 
         //cast everything to String
-        planeString += p.getName() + DELIMITER; //0
-        planeString += p.getColor() + DELIMITER; //1
-//        planeString += Double.toString(p.getMaxSpeed()) + DELIMITER;
-//        planeString += Double.toString(p.getAccel());
-        planeString += p.getMaxSpeed() + DELIMITER; //2
-        planeString += p.getAccel(); //3
+        planeString += p.getName() + DELIMITER;
+        planeString += p.getColor() + DELIMITER;
+        planeString += Double.toString(p.getMaxSpeed()) + DELIMITER;
+        planeString += Double.toString(p.getAccel());
+//        planeString += p.getMaxSpeed() + DELIMITER;
+//        planeString += p.getAccel();
 
         return planeString;
     }
