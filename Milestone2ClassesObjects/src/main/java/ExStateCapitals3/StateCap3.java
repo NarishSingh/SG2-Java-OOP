9daddf8d4d264b9c19ebef8d3245c823c5f2c2b8
Narish Singh <narish.singh786@gmail.com>
2020-05-06 00:01:51 -0400

    Finished the population and area limit returns for StateCapitals3

--- Milestone2ClassesObjects/src/main/java/ExStateCapitals3/StateCap3.java
+++ Milestone2ClassesObjects/src/main/java/ExStateCapitals3/StateCap3.java
@@ -18,22 +18,49 @@ public class StateCap3 {
         Map<String, Capital> stateCapsInfo = new HashMap<>();
 
         Scanner read = new Scanner(new BufferedReader(new FileReader("StateCapitals2.txt")));
-        
+
         while (read.hasNextLine()) {
-            String currentLine=read.nextLine();
-            String[] token=currentLine.split("::");
+            String currentLine = read.nextLine();
+            String[] token = currentLine.split("::");
             stateCapsInfo.put(token[0], new Capital(token[1], Integer.parseInt(token[2]), Double.parseDouble(token[3])));
         }
-        
+
         Set<String> stateInfoList = stateCapsInfo.keySet();
         System.out.println("The US has " + stateInfoList.size() + " states");
 
-        System.out.println("\nCapital List:");
+        System.out.println("\nInformation on State Capitals:");
         for (String s : stateInfoList) {
             System.out.println(s + "'s capital is " + stateCapsInfo.get(s));
         }
-        
-        //TODO finish the pop/area limit prompt
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("\nEnter a population limit and we will list states above this limit: ");
+        int popLimit = input.nextInt();
+
+        for (String s : stateInfoList) {
+            //temp store cap to grab population from obj
+            Capital cap = stateCapsInfo.get(s);
+            int statePop = cap.getPop();
+
+            if (popLimit < statePop) {
+                System.out.println(s + " | " + stateCapsInfo.get(s));
+            }
+        }
+
+        System.out.print("\nEnter an area limit in square miles and we will list states above this limit: ");
+        double areaLimit = input.nextDouble();
+
+        for (String s : stateInfoList) {
+            //temp store cap to grab area from obj
+            Capital cap = stateCapsInfo.get(s);
+            double stateArea = cap.getSqMi();
+
+            if (areaLimit < stateArea) {
+                System.out.println(s + " | " + stateCapsInfo.get(s));
+            }
+        }
+
     }
 
 }
