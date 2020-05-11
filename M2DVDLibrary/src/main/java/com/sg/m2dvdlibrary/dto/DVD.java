9a1ed5614c7923104b5b03cb0b091c8466ba2452
Narish Singh <narish.singh786@gmail.com>
2020-05-11 19:32:21 -0400

    DVD class created

--- M2DVDLibrary/src/main/java/com/sg/m2dvdlibrary/dto/DVD.java
+++ M2DVDLibrary/src/main/java/com/sg/m2dvdlibrary/dto/DVD.java
@@ -7,9 +7,68 @@ MPAA rating
 Director's name
 Studio
 User rating or note (allows the user to enter additional information, e.g., "Good family movie")
-*/
+ */
 package com.sg.m2dvdlibrary.dto;
 
 public class DVD {
-    
+
+    /*fields*/
+    private String title;
+    private String releaseDate;
+    private String director;
+    private String studio;
+    private String mpaaRating;
+    private String userRating;
+
+    /*ctor*/
+    public DVD(String title) {
+        this.title = title;
+    }
+
+    /*getter/setters*/
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    /*read only - getter*/
+    public String getTitle() {
+        return title;
+    }
+
 }
